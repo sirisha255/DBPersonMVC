652bb00834a7d59c793e5f367b10d170754fdb49
Sirisha <Sirisha@DESKTOP-856P7A7>
2022-12-06 11:18:39 +0100

    Add project files.

---
+++ DBPersonMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DBPersonMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
