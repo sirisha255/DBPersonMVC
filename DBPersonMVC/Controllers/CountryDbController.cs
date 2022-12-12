22d8c8d25de051ef945c43fa89f7fff89719fa58
Sirisha <Sirisha@DESKTOP-856P7A7>
2022-12-12 13:20:58 +0100

    Country and Langugages are added and working  without any errors( create and delete)

---
+++ DBPersonMVC/Controllers/CountryDbController.cs
@@ -0,0 +1,47 @@
+﻿using DBPersonMVC.Data;
+using DBPersonMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DBPersonMVC.Controllers
+{
+    public class CountryDbController : Controller
+    {
+        readonly PeopleDbContext _peopleDb;
+        public CountryDbController(PeopleDbContext peopleDb)
+        {
+            _peopleDb = peopleDb;
+
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_peopleDb.Countries.ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Country country)
+        {
+            ModelState.Remove("id");
+            if (ModelState.IsValid)
+            {
+                country.Id = country.Id;
+                _peopleDb.Countries.Add(country);
+                _peopleDb.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int Id)
+        {
+            var countryToRemove = _peopleDb.Countries.Find(Id);
+            if (countryToRemove != null)
+            {
+                _peopleDb.Countries.Remove(countryToRemove);
+                _peopleDb.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DBPersonMVC/Controllers/LanguageDbController.cs
@@ -0,0 +1,47 @@
+﻿using DBPersonMVC.Data;
+using DBPersonMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DBPersonMVC.Controllers
+{
+    public class LanguageDbController : Controller
+    {
+        readonly PeopleDbContext _peopleDb;
+            public LanguageDbController(PeopleDbContext peopleDb)
+            {
+                _peopleDb = peopleDb;
+
+            }
+            [HttpGet]
+            public IActionResult Index()
+            {
+                return View(_peopleDb.Languages.ToList());
+            }
+            public IActionResult Create()
+            {
+                return View();
+            }
+            [HttpPost]
+            public IActionResult Create(Language language)
+            {
+                ModelState.Remove("id");
+                if (ModelState.IsValid)
+                {
+                    language.Id = language.Id;
+                    _peopleDb.Languages.Add(language);
+                    _peopleDb.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            public IActionResult Delete(int Id)
+            {
+                var languageToRemove = _peopleDb.Languages.Find(Id);
+                if (languageToRemove != null)
+                {
+                    _peopleDb.Languages.Remove(languageToRemove);
+                    _peopleDb.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+        }
+}
